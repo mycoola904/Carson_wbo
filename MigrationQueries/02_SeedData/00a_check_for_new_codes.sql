296a61aa6ad8fd6752b3934b44f2079615693a97
mycoola904 <mycoola904@gmail.com>
2025-09-09 11:15:50 -0400

    Add condition to filter out existing service codes in the new codes check query

--- MigrationQueries/02_SeedData/00a_check_for_new_codes.sql
+++ MigrationQueries/02_SeedData/00a_check_for_new_codes.sql
@@ -10,5 +10,7 @@ from auto a
 inner join cust cu on cu.c_id = a.C_ID
 inner join CodeMaster co on co.SVC_CODE = a.SVC_CODE
 inner join CodeCategory cc on cc.CodeCategoryID = co.CODECATEGORYID
+left join ModMigration.dbo.ServiceCodeDetail sd on sd.ServiceCode = co.SVC_CODE_ALPHA
+where sd.id is null
 group by co.SVC_CODE_ALPHA, co.Descript, cc.Description, co.LINK_STAT
 order by co.SVC_CODE_ALPHA
